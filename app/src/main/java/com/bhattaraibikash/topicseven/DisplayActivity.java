aca09adfcbf78d5e12d7a8495bdabfd7a7f4fb16
bethebikash <bhattaraibikash55@gmail.com>
2020-02-29 14:19:21 +0545

    update

--- app/src/main/java/com/bhattaraibikash/topicseven/DisplayActivity.java
+++ app/src/main/java/com/bhattaraibikash/topicseven/DisplayActivity.java
@@ -3,12 +3,24 @@ package com.bhattaraibikash.topicseven;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    private TextView tvMeaning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        tvMeaning= findViewById(R.id.tvMeaning);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle!=null){
+            String meaning = bundle.getString("meaning");
+            tvMeaning.setText(meaning);
+        }
     }
 }
